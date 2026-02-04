e165c77d6f568e18bae3b0c5d98b5ae6e8ceb931
JoseFernandoGuarin <99984540+JoseFernandoGuarin@users.noreply.github.com>
2026-02-04 11:36:07 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,38 @@
+const modelo = {
+  inicio: {
+    hacienda: "Hacienda La Montaña – UdeA",
+    anio: 2025,
+    sistema: "academico",
+    vacas: 120,
+    prodVaca: 22,
+    dias: 365
+  },
+  inventario: {
+    ordeño: 120,
+    secas: 20,
+    novillas: 40,
+    terneras: 35,
+    toros: 2
+  },
+  alimentacion: {
+    forraje: { cant: 12, costo: 450 },
+    concentrado: { cant: 6, costo: 1800 },
+    minerales: { cant: 0.12, costo: 3200 }
+  },
+  manoObra: {
+    empleados: 6,
+    salario: 1800000,
+    prestaciones: 0.35
+  },
+  sanidad: {
+    sanidad: 18000000,
+    reproduccion: 12000000
+  },
+  indirectos: {
+    energia: 9000000,
+    agua: 3500000,
+    mantenimiento: 12000000,
+    combustible: 8000000,
+    administracion: 10000000
+  }
+};
