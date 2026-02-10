58337763585f029457c2501a0a2b5c74a77bc8aa
JoseFernandoGuarin <99984540+JoseFernandoGuarin@users.noreply.github.com>
2026-02-10 11:24:30 -0500

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,11 @@
 console.log("✅ App Hacienda La Montaña cargada correctamente");
 alert("La app se cargó correctamente");
+
+fetch("modules/ingresos.html")
+  .then(r => r.text())
+  .then(html => {
+    document.getElementById("modulo-ingresos").innerHTML = html;
+    const script = document.createElement("script");
+    script.src = "assets/js/ingresos.js";
+    document.body.appendChild(script);
+  });
